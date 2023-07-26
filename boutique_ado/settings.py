ed4267575af0ccaebf8fa39c06f1742e0a8576d8
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-07-26 10:49:44 +0200

    Added basi checkout funtionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'checkout',
     
     # Other
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -170,3 +170,12 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+# STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+# STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
